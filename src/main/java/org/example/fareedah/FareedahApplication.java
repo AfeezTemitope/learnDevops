f681cd654561e86fda43f8cc7f7fe192337a8a8c
AfeezTemitope <belloafeez28@gmail.com>
2025-02-27 14:49:37 +0100

    lesson 1

---
+++ src/main/java/org/example/fareedah/FareedahApplication.java
@@ -0,0 +1,13 @@
+package org.example.fareedah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FareedahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FareedahApplication.class, args);
+    }
+
+}
